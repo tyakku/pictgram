f49d15bbbd4fab9cc306383bc99518355304b643
EC2 Default User <ec2-user@ip-172-31-26-244.us-east-2.compute.internal>
2019-10-15 15:41:42 +0000

    pictgram

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 15}
     validates :email, presence: true
-    validates :password_digest, length:  {minimum: 8, maximum: 32}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :password, length:  {minimum: 8, maximum: 32}, format: {with: /^[A-Za-z0-9]+S/, multiline:  true}
+    
+    
         
     
     has_secure_password
