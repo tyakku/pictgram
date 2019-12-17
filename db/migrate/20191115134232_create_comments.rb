108c0839f94f8e3b740e32ce9180c2a60741dd85
EC2 Default User <ec2-user@ip-172-31-26-244.us-east-2.compute.internal>
2019-12-17 15:42:40 +0000

    first commit

--- db/migrate/20191115134232_create_comments.rb
+++ db/migrate/20191115134232_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
-  create_table :users do |t| 
+  create_table :comments do |t| 
     t.integer :user_id
     t.integer :topic_id
     t.text    :comment
